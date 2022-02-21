e40e52cc2cb9f393627d028d877062a579773171
RosiPratyusha <99171898+RosiPratyusha@users.noreply.github.com>
2022-02-21 15:21:56 +0530

    Add files via upload

---
+++ BoutiqueController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using BoutiqueDataConnection.DataComponents;
+using BoutiqueDataConnection.DataLayer;
+using BoutiqueDataConnection.Entities;
+
+namespace Boutique.Controllers
+{
+    public class BoutiqueController : Controller
+    {
+        // GET: Boutique
+        public ActionResult GetAllProducts()
+        {
+            //Get the Model data
+            var connection= ProductFactory.CreateComponent(); 
+            var model = connection.GetAllProducts();
+            //Send it to the view
+            return View(model);
+        }
+       
+        public ActionResult UpdateProduct(string id)
+        {
+            int productId = Convert.ToInt32(id);
+            var connection = ProductFactory.CreateComponent();
+            try
+            {
+                return View();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        public ActionResult UpdateProduct(ProductClass product)
+        {
+            var component = ProductFactory.CreateComponent();
+            try
+            {
+                component.UpdateProduct(product);
+                return RedirectToAction("GetAllProducts");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult AddNewProduct()
+        {
+            return View(new ProductClass());
+        }
+        [HttpPost]
+        public ActionResult AddNewProduct(ProductClass product)
+        {
+            var connection = ProductFactory.CreateComponent();
+            try
+            {
+                connection.AddNewProduct(product);
+                //throw new Exception("Testing Error!!!");
+                return RedirectToAction("GetAllProducts");
+                //return View(new ProductClass());
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                ViewBag.ErrorMessage = message;
+                return View(new ProductClass());
+            }
+        }
+    }
+}
